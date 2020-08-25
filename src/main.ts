600d4f061c5c21629d06fb6e53b7f9358fac484a
Joel Male <joel@joelmale.com>
2020-08-26 09:50:21 +1000

    Small fixes to action.yml and action output

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core';
 import { http } from './http';
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
     const url = core.getInput('url');
@@ -15,13 +14,11 @@ async function run() {
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
     // make the request
-    http.make(url, headers, body).then((res) => console.log('hi'));
+    http.make(url, headers, body)
+      .then((res) => core.setOutput('statusCode', res.status));
 
     // debug end
     core.info((new Date()).toTimeString());
-
-    // output the time it took
-    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
   }
