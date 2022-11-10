1143d5b401fb8f0f3a003f1b3ac676550ba69473
Joel Male <joel@joelmale.com>
2022-11-10 22:40:17 +1000

    wip

--- dist/index.js
+++ dist/index.js
@@ -6937,21 +6937,18 @@ exports.debug = debug; // for test
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.http = void 0;
-var fetch = __nccwpck_require__(4429);
+const fetch = __nccwpck_require__(4429);
 var https = __nccwpck_require__(5687);
-var Http = (function () {
-    function Http() {
-    }
-    Http.prototype.make = function (url, headers, body, ignoreCertificate) {
-        var _this = this;
-        return new Promise(function (resolve, reject) {
-            fetch(url, _this.getOptions('post', headers, body, ignoreCertificate)).then(function (res) { return resolve(res); });
+class Http {
+    make(url, headers, body, ignoreCertificate) {
+        return new Promise((resolve, reject) => {
+            fetch(url, this.getOptions('post', headers, body, ignoreCertificate)).then((res) => resolve(res));
         });
-    };
-    Http.prototype.getOptions = function (method, headers, body, ignoreCertificate) {
-        var options = {
+    }
+    getOptions(method, headers, body, ignoreCertificate) {
+        const options = {
             headers: headers ? JSON.parse(headers) : {},
-            method: method
+            method
         };
         if (body) {
             options.body = body;
@@ -6961,9 +6958,8 @@ var Http = (function () {
         }
         options.headers['content-type'] = 'application/json';
         return options;
-    };
-    return Http;
-}());
+    }
+}
 exports.http = new Http();
 
 
@@ -6983,83 +6979,52 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-var core = __nccwpck_require__(2186);
-var http_1 = __nccwpck_require__(9047);
+const core = __nccwpck_require__(2186);
+const http_1 = __nccwpck_require__(9047);
 function run() {
-    return __awaiter(this, void 0, void 0, function () {
-        var url, headers, body, insecure;
-        return __generator(this, function (_a) {
-            url = core.getInput('url')
-                ? core.getInput('url')
-                : process.env.WEBHOOK_URL
-                    ? process.env.WEBHOOK_URL
-                    : '';
-            headers = core.getInput('headers')
-                ? core.getInput('headers')
-                : process.env.headers
-                    ? process.env.headers
-                    : null;
-            body = core.getInput('body')
-                ? core.getInput('body')
-                : process.env.data
-                    ? process.env.data
-                    : null;
-            insecure = core.getInput('insecure')
-                ? core.getInput('insecure') == 'true'
-                : process.env.insecure
-                    ? process.env.insecure == 'true'
-                    : false;
-            if (!url) {
-                core.setFailed('A url is required to run this action.');
-                throw new Error('A url is required to run this action.');
-            }
-            http_1.http
-                .make(url, headers, body, insecure)
-                .then(function (res) {
-                if (res.status >= 400) {
-                    error(res.status);
-                    return;
-                }
-            })
-                .catch(function (err) {
-                error(err.status);
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = core.getInput('url')
+            ? core.getInput('url')
+            : process.env.WEBHOOK_URL
+                ? process.env.WEBHOOK_URL
+                : '';
+        const headers = core.getInput('headers')
+            ? core.getInput('headers')
+            : process.env.headers
+                ? process.env.headers
+                : null;
+        const body = core.getInput('body')
+            ? core.getInput('body')
+            : process.env.data
+                ? process.env.data
+                : null;
+        const insecure = core.getInput('insecure')
+            ? core.getInput('insecure') == 'true'
+            : process.env.insecure
+                ? process.env.insecure == 'true'
+                : false;
+        if (!url) {
+            core.setFailed('A url is required to run this action.');
+            throw new Error('A url is required to run this action.');
+        }
+        http_1.http
+            .make(url, headers, body, insecure)
+            .then(res => {
+            if (res.status >= 400) {
+                error(res.status);
                 return;
-            });
-            return [2];
+            }
+        })
+            .catch(err => {
+            error(err.status);
+            return;
         });
     });
 }
 function error(statusCode) {
-    core.setFailed("Received status code: ".concat(statusCode));
-    throw new Error("Request failed with status code: ".concat(statusCode));
+    core.setFailed(`Received status code: ${statusCode}`);
+    throw new Error(`Request failed with status code: ${statusCode}`);
 }
 run();
 
